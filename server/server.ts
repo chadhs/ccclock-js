7ad9054b60a8d9ace56c7102ccd5e189872fe255
Chad Stovern <chad@stovern.me>
2023-07-04 08:38:24 -0500

    cleanup, server serve up client, setup build, start combined dev

--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,13 @@
 import express from "express";
+import path from "path";
 import "dotenv/config";
 import axios from "axios";
 
 const app = express();
 
 const apiKey = process.env.OPENWEATHER_API_KEY;
+const LAT = process.env.LAT || 42.867;
+const LON = process.env.LON || -88.333;
 const apiUrl = "https://api.openweathermap.org/data/3.0/onecall";
 
 async function getWeatherData() {
@@ -12,8 +15,8 @@ async function getWeatherData() {
     const response = await axios.get(apiUrl, {
       params: {
         appid: apiKey,
-        lat: 42.867,
-        lon: -88.333,
+        lat: LAT,
+        lon: LON,
       },
     });
     return response.data;
@@ -24,14 +27,15 @@ async function getWeatherData() {
 
 app.get("/api/weather", async (req, res) => {
   const weatherData = await getWeatherData();
-  const mockData = {
-    temp: 80,
-    hi: 82,
-    low: 68,
-  };
   res.json(weatherData);
 });
 
-app.listen(5001, () => {
-  console.log("Server started on port 5001");
+app.use(express.static("../client/build"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "..", "client", "build", "index.html"));
+});
+
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => {
+  console.log("Server started on port ", PORT);
 });
